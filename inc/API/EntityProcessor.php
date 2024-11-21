ee190f0ba3a031bcfcb620bc7a96456862bc9d0f
michelmany <hello@michelmany.com>
2024-11-21 22:06:52 +0100

    set first photo as featured image

--- inc/API/EntityProcessor.php
+++ inc/API/EntityProcessor.php
@@ -10,11 +10,22 @@ abstract class EntityProcessor {
 		update_field( 'bathrooms', $entity->baths, $wpEntityPost->ID );
 		update_field( 'square_footage', $entity->squareFootage, $wpEntityPost->ID );
 		update_field( 'address', $this->prepareAddress( $entity ), $wpEntityPost->ID );
-		update_field( 'image_gallery', $this->uploadImages( $entity->photos ), $wpEntityPost->ID );
 		update_field( 'description', $entity->description, $wpEntityPost->ID );
 		update_field( 'schedule_viewing_url', $entity->btnUrl, $wpEntityPost->ID );
 		update_field( 'submit_an_application_url', $entity->applyUrl, $wpEntityPost->ID );
 		update_field( 'restrictions', '', $wpEntityPost->ID ); // Empty field
+
+		// Set the first photo as the featured image
+		if ( ! empty( $entity->photos ) ) {
+			$featuredImageId = $this->uploadImage( $entity->photos[0] );
+			if ( $featuredImageId ) {
+				set_post_thumbnail( $wpEntityPost->ID, $featuredImageId );
+			}
+
+			// Upload the rest of the images
+			$imageIds = $this->uploadImages( array_slice( $entity->photos, 1 ) );
+			update_field( 'image_gallery', $imageIds, $wpEntityPost->ID );
+		}
 	}
 
 	protected function prepareAddress( $entity ): array {
